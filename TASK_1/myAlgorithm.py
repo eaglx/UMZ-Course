bed367ff1d41e23ce75bb1828439bce14207c4d4
eaglx <eaglx.programmer@gmail.com>
2019-03-24 13:03:07 +0100

    Update solution

--- TASK_1/myAlgorithm.py
+++ TASK_1/myAlgorithm.py
@@ -14,11 +14,16 @@ def load_train_data(fileName):
             if(count == 0):
                 count += 1
                 continue
-            loadDataField.append(line.split()[1])
-            loadDataPrice.append(line.split()[0])
+            loadDataField.append(float(line.split()[1]))
+            loadDataPrice.append(float(line.split()[0]))
     loadData = []
     loadData.append(loadDataField) # oś-X
     loadData.append(loadDataPrice) # oś-Y
+
+    # print(loadDataField)
+    # print(loadDataPrice)
+    # exit()
+
     return loadData
 # ********************************************
 
@@ -33,19 +38,29 @@ def J(h, theta, x, y):
     return 1.0 / (2 * m) * sum((h(theta, x[i]) - y[i])**2 for i in range(m))
 # ********************************************
 
-# Oblicz wartość funkcji kosztu
-def regline2(fig, fun, theta, xx, yy):
-    ax = fig.axes[0]
-    x0, x1 = min(xx), max(xx)
-    X = [x0, x1]
-    Y = [fun(theta, x) for x in X]
-    cost = J(fun, theta, xx, yy)
-    ax.plot(X, Y, linewidth='2',
-            label=(r'$y={theta0}{op}{theta1}x, \; J(\theta)={cost:.3}$'.format(
-                theta0=theta[0],
-                theta1=(theta[1] if theta[1] >= 0 else -theta[1]),
-                op='+' if theta[1] >= 0 else '-',
-                cost=cost)))
+# Gradient Descent
+def gd(h, costfun, theta, x, y, alpha, eps):
+    current_cost = costfun(h, theta, x, y)
+    log = [[current_cost, theta]]
+    m = len(y)
+    while True:
+        new_theta = [
+        theta[0] - alpha/float(m) * sum(h(theta, x[i]) - y[i] for i in range(m)),
+        theta[1] - alpha/float(m) * sum((h(theta, x[i]) - y[i]) * x[i] for i in range(m))
+        ]
+        theta = new_theta
+        try:
+            current_cost, prev_cost = costfun(h, theta, x, y), current_cost
+        except OverflowError:
+            break
+
+        if abs(prev_cost - current_cost) <= eps:
+            break
+        log.append([current_cost, theta])
+        # print(log[-1])
+    # print(theta)
+    # print(log)
+    return log[-1][0]
 # ********************************************
 
 # Funkcja sterójąca
@@ -53,6 +68,23 @@ def main():
     print("ALGORITHM-TASK-SOLVER")
     loadTrainDataToProcessing = load_train_data(sys.argv[1])
 # ********************************************
+    current_cost = 0.0
+    prev_cost = gd(h, J, [0, 0], loadTrainDataToProcessing[0], loadTrainDataToProcessing[1], alpha=0.001, eps=0.01)
+    eps = 0.01 / 2
+    alpha = 0.001
+    while True:
+        current_cost = gd(h, J, [0, 0], loadTrainDataToProcessing[0], loadTrainDataToProcessing[1], alpha, eps)
+        if current_cost < prev_cost:
+            prev_cost = current_cost
+            current_cost = 0.0
+            eps = eps / 2
+        elif prev_cost == -1.0:
+            prev_cost = current_cost
+            current_cost = 0.0
+            eps -= 0.0001
+        else:
+            break
+    print(prev_cost)
 
 if __name__ == "__main__":
     main()

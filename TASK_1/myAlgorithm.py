1ae6c5b85da3047c5bd982eb0fc5003da894829c
eaglx <eaglx.programmer@gmail.com>
2019-03-25 16:41:52 +0100

    Update TASK_1

--- TASK_1/myAlgorithm.py
+++ TASK_1/myAlgorithm.py
@@ -14,17 +14,20 @@ def load_train_data(fileName):
             if(count == 0):
                 count += 1
                 continue
-            loadDataField.append(float(line.split()[1]))
-            loadDataPrice.append(float(line.split()[0]))
+            loadDataField.append(float(line.split("\t")[1]))
+            loadDataPrice.append(float(line.split("\t")[0]))
     loadData = []
     loadData.append(loadDataField) # oś-X
     loadData.append(loadDataPrice) # oś-Y
-
-    # print(loadDataField)
-    # print(loadDataPrice)
-    # exit()
-
     return loadData
+
+def load_data(fileName):
+    print("> LOAD DEV DATA")
+    loadDataField = []
+    with open(fileName) as f:
+        for line in f.readlines():
+            loadDataField.append(float(line.split("\t")[0]))
+    return loadDataField
 # ********************************************
 
 # Funkcja liniowa jednej zmiennej
@@ -43,7 +46,10 @@ def gd(h, costfun, theta, x, y, alpha, eps):
     current_cost = costfun(h, theta, x, y)
     log = [[current_cost, theta]]
     m = len(y)
+    max_itern = 500000
+    count_itern = 0
     while True:
+        count_itern += 1
         new_theta = [
         theta[0] - alpha/float(m) * sum(h(theta, x[i]) - y[i] for i in range(m)),
         theta[1] - alpha/float(m) * sum((h(theta, x[i]) - y[i]) * x[i] for i in range(m))
@@ -51,40 +57,35 @@ def gd(h, costfun, theta, x, y, alpha, eps):
         theta = new_theta
         try:
             current_cost, prev_cost = costfun(h, theta, x, y), current_cost
+            # print("diff: ",prev_cost - current_cost)
         except OverflowError:
             break
 
         if abs(prev_cost - current_cost) <= eps:
             break
         log.append([current_cost, theta])
-        # print(log[-1])
-    # print(theta)
-    # print(log)
-    return log[-1][0]
+
+        if count_itern == max_itern:
+            break
+    #     print(current_cost, prev_cost)
+    # print(log[-1])
+    return log
 # ********************************************
 
 # Funkcja sterójąca
 def main():
     print("ALGORITHM-TASK-SOLVER")
     loadTrainDataToProcessing = load_train_data(sys.argv[1])
-# ********************************************
-    current_cost = 0.0
-    prev_cost = gd(h, J, [0, 0], loadTrainDataToProcessing[0], loadTrainDataToProcessing[1], alpha=0.001, eps=0.01)
-    eps = 0.01 / 2
-    alpha = 0.001
-    while True:
-        current_cost = gd(h, J, [0, 0], loadTrainDataToProcessing[0], loadTrainDataToProcessing[1], alpha, eps)
-        if current_cost < prev_cost:
-            prev_cost = current_cost
-            current_cost = 0.0
-            eps = eps / 2
-        elif prev_cost == -1.0:
-            prev_cost = current_cost
-            current_cost = 0.0
-            eps -= 0.0001
-        else:
-            break
-    print(prev_cost)
+# **************** TRAIN ****************************
+    print("> TRAIN ALGORITHM")
+    prev_cost = gd(h, J, [0, 0], loadTrainDataToProcessing[0], loadTrainDataToProcessing[1], alpha=0.000001, eps=1000)
+    bestTheta = prev_cost[-1][1]
+# ***************************************************
+    loadTrainDataToProcessing = load_data(sys.argv[2])
+    print("> SAVE OUT")
+    with open(sys.argv[3], "w") as f:
+        for i in loadTrainDataToProcessing:
+            f.write(str(h(bestTheta, i)) + "\n")
 
 if __name__ == "__main__":
     main()

bd25b5f004ad855219512a94c7cc7166bbd96f3e
eaglx <eaglx.programmer@gmail.com>
2019-05-10 11:26:52 +0200

    Update TASK_%

---
+++ TASK_5/neural_network_solution.py
@@ -0,0 +1,44 @@
+import sys
+import pandas
+from sklearn.neural_network import MLPClassifier
+
+if len(sys.argv) < 4:
+    print("Not enough args!")
+    print("python3 neural_network_solution.py train.tsv in.tsv out.tsv")
+    exit()
+# ***************** SETUP *****************
+train_file = sys.argv[1]
+in_file = sys.argv[2]
+out_file = sys.argv[3]
+columns_in_file = ['PassengerId','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked']
+train_columns =['Survived','PassengerId','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked']
+columns_to_get_solve = ['PassengerId','Pclass','Sex','Age','SibSp','Fare','Embarked']
+# ***************** READ DATA *****************
+train_tsv = pandas.read_csv(train_file, sep='\t', header=0, usecols=train_columns)
+in_tsv = pandas.read_csv(in_file, sep='\t', header=None, names=columns_in_file)
+# ***************** SOLVE *****************
+x = train_tsv[columns_to_get_solve]
+y = train_tsv['Survived']
+x = x.fillna(0)
+x['Sex'] = x['Sex'].replace('male',1)
+x['Sex'] = x['Sex'].replace('female',0)
+x['Embarked'] = x['Embarked'].replace('S',1)
+x['Embarked'] = x['Embarked'].replace('Q',2)
+x['Embarked'] = x['Embarked'].replace('C',3)
+
+x_in = in_tsv[columns_to_get_solve]
+x_in = x_in.fillna(0)
+x_in['Sex'] = x_in['Sex'].replace('male',1)
+x_in['Sex'] = x_in['Sex'].replace('female',0)
+x_in['Embarked'] = x_in['Embarked'].replace('S',1)
+x_in['Embarked'] = x_in['Embarked'].replace('Q',2)
+x_in['Embarked'] = x_in['Embarked'].replace('C',3)
+
+neural = MLPClassifier()
+neural.fit(x, y)
+test_out = neural.predict(x_in)
+# ***************** SAVE *****************
+outFile = open(out_file, "w")
+for l in test_out:
+	outFile.write('%01d\n' % l)
+outFile.close()
